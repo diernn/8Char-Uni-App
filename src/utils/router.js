10c2dec914605d4923c1167e5e81ac6d21bba2e0
stoat <stoat@stoatme.com>
2026-04-06 00:27:18 +0800

    新增(工具): 新增AI相关路由方法和通用路由工具函数

--- src/utils/router.js
+++ src/utils/router.js
@@ -13,3 +13,58 @@ export const toDetail = () => {
         url: '/pages/detail/index'
     });
 }
+
+export const toAI = () => {
+    route({
+        type: 'navigateTo',
+        url: '/pages/ai/index'
+    });
+}
+
+export const toHomeWithTime = params => {
+    route({
+        type: 'redirect',
+        url: `/pages/home/home?time=${params.time}&gender=${params.gender}`
+    });
+}
+
+export const toAIWithTime = params => {
+    route({
+        type: 'navigateTo',
+        url: `/pages/ai/index?time=${params.time}&gender=${params.gender}`
+    });
+}
+
+export const back = () => {
+    uni.navigateBack()
+}
+
+export const redirectTo = url => {
+    route({
+        type: 'redirect',
+        url,
+    });
+}
+
+export const navigateTo = url => {
+    route({
+        type: 'navigateTo',
+        url,
+    });
+}
+
+export const replaceTo = redirectTo
+
+export default {
+    toHome,
+    toDetail,
+    toAI,
+    toHomeWithTime,
+    toAIWithTime,
+    back,
+    redirectTo,
+    navigateTo,
+    replaceTo,
+}
+
+export { route }
